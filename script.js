08c905bc3f54cf79507074e6d8a147f4bb9dcd4f
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2024-02-28 19:44:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+ document.addEventListener('DOMContentLoaded', function () {
+            const squares = document.querySelectorAll('.square');
+
+            squares.forEach(square => {
+                square.addEventListener('mouseover', function () {
+                    square.style.backgroundColor = 'skyblue';
+                });
+
+                square.addEventListener('mouseout', function () {
+                    setTimeout(() => {
+                        square.style.backgroundColor = 'lightgray';
+                    }, 1000);
+                });
+            });
+        });
